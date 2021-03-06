6389fe5d6bd69b1f80f86be8e492b2b2e5464115
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-03-06 16:30:51 +0200

    added some styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Chart from './components/Chart/Chart'
 import CountryPicker from './components/CountryPicker/CountryPicker'
 import styles from './App.module.css'
 import {fetchData} from './api'
+import corona from './images/image.png'
 
 class App extends Component {
   state ={
@@ -28,6 +29,7 @@ class App extends Component {
     const {data, country} = this.state;
     return ( 
       <div className={styles.container}>
+        <img className={styles.image} src={corona} alt="Covid-19"/>
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country}/>
